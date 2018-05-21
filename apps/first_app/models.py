3a0632a0dc19bb153469fc4e7a62004760e43b00
isi-frischmann <isabell.frischmann@gmail.com>
2018-05-21 14:33:59 -0700

    first upload

---
+++ apps/first_app/models.py
@@ -0,0 +1,77 @@
+from __future__ import unicode_literals
+from django.db import models
+from datetime import date 
+import re
+import bcrypt
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]+$')
+
+class UserManager(models.Manager):
+    def register_validator(self, postData):
+        errors = {}
+        # set variable today to today too check the birthdate
+        today = str(date.today())
+        print(today)
+
+        if len(postData['fname']) < 2:
+            errors['fname'] = 'First name needs to have more then 2 characters'
+        
+        if len(postData['lname']) < 2:
+            errors['lname'] = 'Last name is too short. Needs to have at least two characters'
+
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = 'Email needs to be a propper email address'
+        
+        # Check if email already exist!!!!
+        # use .email to check the email cell in the DB!!!
+        # if User.objects.get(email = postData['email']).email:
+        #     errors['email_exist'] = 'Use different email address'
+        
+        if len(postData['email']) > 1:
+            user_email = User.objects.all()
+            if user_email == postData['email']:
+                errors['email_exist'] = 'Use different email address'
+
+
+        if str(postData['birthDate']) >= today:
+                errors['bdate'] = 'Invalid birthdate - please provide valid birthdate'
+
+        if len(postData['password']) < 8:
+            errors['password'] = 'Password needs to have more then eigth characters'
+        
+        if postData['password'] != postData['c_pw']:
+            errors['c_pw'] = 'Confirmation Password needs to match your Password'
+        return errors
+
+    def login_validator(self, postData):
+        errors = {}
+
+        # check if email matches regex requirements
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = 'Invalid email address'
+         
+        # check if email is greater then 0. And compare with the len of the user mail addresses stored in the DB.
+        if len(User.objects.filter(email = postData['email'])) != 0:
+            # get all users with the email address and check
+            password = User.objects.get(email = postData['email']).password
+            # check if password matches the bcrypt password in the DB. don't forget variable.encode()) != True
+            if bcrypt.checkpw(postData['password'].encode(), password.encode()) != True:
+                errors['password'] = 'Password is not correct'
+        else: 
+            errors['email_not_exists'] = 'No email address found you need to register first'
+        return errors
+
+class User(models.Model):
+    fname = models.CharField(max_length = 40)
+    lname = models.CharField(max_length = 40)
+    email = models.CharField(max_length = 40)
+    b_date = models.CharField(max_length = 10)
+    password = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    objects = UserManager()
+
+    def __repr__(self):
+        return '<Object User: {} {} {} {} {}>'.format(self.fname, self.lname, self.email, self.b_date, self.password)
+
---
+++ apps/first_app/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import User
+import bcrypt
+from datetime import date
+
+def index(request):
+    return render(request, 'first_app/index.html')
+
+
+def process(request):
+    # print(request.POST.get('action'))
+    # using this if statement as there are two forms in the HTML file.
+    # The submit button is hidden so you're able to use request.POST.get for register and login
+    if request.POST.get('action') == 'registration':
+        errors = User.objects.register_validator(request.POST)
+        # check if there are any error messages to send
+        if len(errors):
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            hash_userpw = bcrypt.hashpw(str(request.POST['password']).encode(), bcrypt.gensalt())
+            # print(hash)
+            # add the user into the DB
+            user = User.objects.create()
+            user.fname = request.POST['fname']
+            user.lname = request.POST['lname']
+            user.email = request.POST['email']
+            user.b_date = request.POST['birthDate']
+            user.password = hash_userpw
+            user.save()
+            # get the email info from the user
+            email = request.POST['email']
+            # search the fname in the DB where the email == email from the user (given from the form)
+            # then you can check with email address belongs to which user firstname!!
+            # at the end .fname gives you just the fname cell from the DB instead the whole user info
+            request.session['fname'] = User.objects.get(email = email).fname
+            return redirect('/success')
+
+    if request.POST.get('action') == 'login':
+        errors = User.objects.login_validator(request.POST)
+        if len(errors):
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        email = request.POST['email']
+        request.session['fname'] = User.objects.get(email = email).fname
+        return redirect('/success')
+    
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def success(request):
+    return render(request, 'first_app/success.html')
